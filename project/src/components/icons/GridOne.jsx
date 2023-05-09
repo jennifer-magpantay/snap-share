ec97949e3ab0e92f111d0e0d3426ae24afecdbed
Jennifer Magpantay <56962997+jennifer-magpantay@users.noreply.github.com>
2023-05-09 16:44:33 +0200

    Create GridOne.jsx

---
+++ project/src/components/icons/GridOne.jsx
@@ -0,0 +1,15 @@
+const GridOne = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 38 38"
+    width="50"
+    height="50"
+    fill="none"
+  >
+    <path
+      stroke="#000"
+      d="M.5.5h15.667v15.667H.5V.5Zm0 20.833h15.667V37H.5V21.333ZM21.333.5H37v15.667H21.333V.5Zm0 20.833H37V37H21.333V21.333Z"
+    />
+  </svg>
+);
+export default GridOne;
